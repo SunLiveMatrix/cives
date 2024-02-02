20cb419543b6307b3f11c820f1aadbe8a148b06c
SunLiveMatrix <152664029+SunLiveMatrix@users.noreply.github.com>
2024-02-01 21:38:30 -0300

    cives
    Codux is an all-in-one visual development environment. Whether you’re starting a new app, creating components, or editing an existing project, every visual change you make reflects instantly in the code and vice versa. To learn more about Codux, visit our website - [https://www.codux.com/](https://www.codux.com/)

---
+++ matrix/gnu/bin/cives.d
@@ -0,0 +1,12 @@
+module matrix.gnu.bin.cives;
+
+import std.stdio;
+
+void values(double x, double y, double z) {
+    return values(x, y, z);
+}
+
+void main(string[] args)
+{
+    writeln(args[0]);
+}
